480201d93bb962affd405b2c49025ee102dae72b
wpreachuk <wpreachuk@DESKTOP-S4T2BBJ.localdomain>
2018-10-21 17:31:07 -0400

    Getting Github Site Running

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
+app.get('/index.html',function (req,res){
+    res.render('index')
+})
+
+app.post('/index.html',function (req,res){
+    console.log(req.body.searchdata);
+    res.render('index');
+  })
+
 app.get('/', function (req, res) {
   res.render('index')
 })
