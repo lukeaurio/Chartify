66b10c4a3201eba376a41d9071d3ddc7e5092df4
wpreachuk <wpreachuk@DESKTOP-S4T2BBJ.localdomain>
2018-10-21 17:04:36 -0400

    ChartJS Install plus Github Site config

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
+app.get('/index.html', function (req, res) {
   res.render('index')
 })
 
-app.post('',function(req,res){
-    res.render('index')
+app.post('/index.html',function(req,res){
+  console.log(req.body.city);
+  res.render('index');
 })
 
 app.listen(3000, function () {
