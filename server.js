69ffd7c8590794f94574181a5bdf0a58e666c472
wpreachuk <wpreachuk@github.com>
2018-10-27 11:59:39 -0400

    Reverted smaller changes, small refactor

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const charts = require('chartjs');
+const charts = require('chart.js');
 const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
