8a986f1f9879d701cf9c27109fd6e4e2200a34c4
wpreachuk <wpreachuk@github.com>
2018-11-10 14:35:51 -0500

    added initial attempts at paging for track retrieval.

--- SpotifyHandlers/SpotifyRetriever.js
+++ SpotifyHandlers/SpotifyRetriever.js
@@ -1,6 +1,28 @@
 const request = require('request');
 const auth = require('./SpotifyAuth')
 
+async function _getAllTracks(page){
+    
+}
+function _getNextTrack(page){
+    var token = await auth.getBearerAuthToken()
+    var page
+    if(page.next){
+    return new Promise(function(resolve, reject) {
+        request.get( 
+            options = {
+                uri:page.next,
+                headers:{Authorization:token},
+                json:true
+            }, (err, res, body) => {
+            if (err) {  console.log(err); return reject(err); }
+            pageResult = body
+            resolve(pageResult) 
+            });
+        }
+    }
+}
+
 module.exports = {
     GetAlbum: async function (AlbumID){
         var token = await auth.getBearerAuthToken()
