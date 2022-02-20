d699d62f657ac8b09206aa4cb8e9c1209669ae4f
Will Preachuk <will.preachuk@gmail.com>
2022-02-20 10:51:26 -0500

    Archive of old NPM Packages

---
+++ Old_Version_Node/README.md
@@ -0,0 +1,7 @@
+# Chartify
+A simple app used to show the Emotional flow of albums and playlists
+Based off of an Idea by RCharlie https://www.rcharlie.com/post/fitter-happier/
+Will connect to Spotify and use Chart.js in order to create and build charts
+Can be used to graph and analyze albums, songs and (eventually) playlists 
+for their emotional highs and lows for proper playlist development.
+will eventually allow creation of playlists via Spotify's User API
---
+++ Old_Version_Node/SpotifyHandlers/SpotifySearch.js
@@ -0,0 +1,28 @@
+const request = require('request');
+const auth = require('./SpotifyAuth')
+
+module.exports = {
+    AlbumSearch: async function (searchParams){
+        var token = await auth.getBearerAuthToken()
+        searchParams.replace(" ","%20")
+        var albumResults
+        return new Promise(function(resolve, reject) {
+        request.get( 
+            options = {
+                uri:'https://api.spotify.com/v1/search',
+                headers:{Authorization:token},
+                qs:{
+                    q:searchParams,
+                    type:'album',
+                    limit:10
+                    },
+                json:true
+            }, (err, res, body) => {
+            if (err) {  console.log(err); return reject(err); }
+            albumResults = body
+            resolve(albumResults) 
+            });
+        });
+    }
+};
+
