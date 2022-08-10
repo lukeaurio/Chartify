d38420215eab47c794802077f81e83fc1e8d4957
wpreachuk <will.preachuk@gmail.com>
2022-08-10 00:30:25 -0400

    Location for old node code that's done

--- Python_Implementation/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import spotify_service as s
-app = Flask(__name__)
-
-@app.route("/")
-def root_route():
-    return "Hello Wurld!"
-
--- Python_Implementation/helpers.py
+++
@@ -1,4 +0,0 @@
-import json
-
-def dumpIt(obj):
-    return json.dumps(obj,indent=4,sort_keys=True,default=str)
