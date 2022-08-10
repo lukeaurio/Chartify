10f85046bb41c10d225de551d2ff62fc5bdb879f
wpreachuk <will.preachuk@gmail.com>
2022-08-10 03:39:41 -0400

    sample chart create

--- Python_Implementation/main.py
+++ Python_Implementation/main.py
@@ -1,12 +1,30 @@
 import spotify_service
 import os
 import helpers as h
+import plotly.io as pio
+import plotly.graph_objects as go
+
 def main():
+    pio.renderers.default = 'browser'
     sp = spotify_service.SpotifyService()
-    x = sp.get_all_user_playlists(user_id = os.getenv("SPOTIPY_USER"))
-    print(h.dumpIt(x[0].toDict()))
+    playlists = sp.get_all_user_playlists(user_id = os.getenv("SPOTIPY_USER"))
+    print(h.dumpIt(playlists[0].toDict()))
 
+    fig = go.Figure()
 
+    fig.add_trace(go.Scatter(
+        x = playlists[0].track_names,
+        y = [i.energy for i in playlists[0].trackStats],
+        name = "energy",
+        line =dict(color='royalblue', width=4),
+        connectgaps=True
+        )
+    )
+
+    fig.update_layout(title=f"Flow of {playlists[0].Name}",
+                   xaxis_title='Track Title',
+                   yaxis_title='Value')
+    fig.show()
 
     print("fin")
 
